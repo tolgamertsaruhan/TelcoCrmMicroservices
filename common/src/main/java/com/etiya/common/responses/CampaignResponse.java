34a8edffc255d85f293542b0c4496f43bd4d632a
tolga.saruhan <tolgamertsaruhan@outlook.com>
2025-11-10 17:42:25 +0300

    updated basketservice unversioned files

---
+++ common/src/main/java/com/etiya/common/responses/CampaignResponse.java
@@ -0,0 +1,62 @@
+package com.etiya.common.responses;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class CampaignResponse {
+    private UUID id;
+    private String name;
+    private String campaignCode;
+    private BigDecimal discountRate;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCampaignCode() {
+        return campaignCode;
+    }
+
+    public void setCampaignCode(String campaignCode) {
+        this.campaignCode = campaignCode;
+    }
+
+    public BigDecimal getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(BigDecimal discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+}
